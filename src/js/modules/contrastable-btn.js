5976e8d6589e0ba22273c150d6a48568f7f443a1
Viseko <viseko23@gmail.com>
2022-09-08 22:14:07 +0700

    Refactored fixed-buttons code & renamed index.js to app.js

--- src/js/modules/contrastable-btn.js
+++ src/js/modules/contrastable-btn.js
@@ -1,109 +1,71 @@
-const defaultColor = "rgb(46, 73, 96)";
+import { getElemFromPoint, mathElemBackground } from "./helpers.js";
+
+const defaultColor = "#2e4960";
+
+const getSectionType = (section) => {
+  return mathElemBackground(section, defaultColor) ? "contrast" : "default";
+};
 
 const fillButton = (options) => {
-  const { btn, top, bottom, bottomOffset } = options;
+  const { btn, top, bottom, offset } = options;
 
   const bgContrast = btn.querySelector(".contrastable-btn__bg--contrast");
 
   if (top === "contrast") {
     bgContrast.style.top = "0px";
     bgContrast.style.bottom = "unset";
-    bgContrast.style.height = bottomOffset ? `${bottomOffset}px` : "100%";
+    bgContrast.style.height = offset ? `${offset}px` : "100%";
 
     if (bottom === "default") {
-      bgContrast.style.height = bottomOffset ?
-        `calc(100% - ${bottomOffset}px)` :
-        "0";
+      bgContrast.style.height = offset ? `calc(100% - ${offset}px)` : "0";
     }
   } else {
     bgContrast.style.top = "unset";
     bgContrast.style.bottom = "0px";
-    bgContrast.style.height = bottomOffset ? `${bottomOffset}px` : "0";
+    bgContrast.style.height = offset ? `${offset}px` : "0";
   }
 };
 
-const checkCoords = (options) => {
-  const { btn, topY, bottomY } = options;
-
-  // 1. Определение фоновых секций снизу и сверху
-  const topSection = document
-    .elementFromPoint(10, topY)
-    .closest("section, footer");
-
-  const bottomSection = document
-    .elementFromPoint(10, bottomY)
-    .closest("section, footer");
-
-  // 2. Если верхняя === нижняя:
-  if (topSection === bottomSection) {
-    // -- 2.1. Определяем контраст или дефолт
-    const sectionColor = getComputedStyle(topSection).backgroundColor;
-    const isContrast = sectionColor === defaultColor;
-    const color = isContrast ? "contrast" : "default";
-
-    // -- 2.2. Перекрашиваем кнопку в соответствующую заливку
-    fillButton({
-      btn,
-      top: color,
-      bottom: color,
-      bottomOffset: 0,
-    });
-  } else {
-    // 3. Если верхняя !== нижняя:
-    // -- 3.1. Определяем цвета обеих
-    const topSectionColor = getComputedStyle(topSection).backgroundColor;
-    const bottomSectionColor = getComputedStyle(bottomSection).backgroundColor;
-
-    if (topSectionColor === bottomSectionColor) {
-      // -- 3.2. Цвета равны? --> Определяем, контрастный или нет
-      // -- -- 3.2.1. Перекрашиваем кнопку в соотв. заливку
-      const isContrast = topSectionColor === defaultColor;
-      const color = isContrast ? "contrast" : "default";
-
-      fillButton({
-        btn,
-        top: color,
-        bottom: null,
-        bottomOffset: 0,
-      });
-    } else {
-      // -- 3.3. Цвета не равны? --> Определяем, насколько выступает нижний элемент
-      // -- -- 3.3.1. Меняем высоту и положение фонов в зависимости от выбранного цвета и порога заступа
-      const bottomSectionTop = bottomSection.getBoundingClientRect().top;
-      const offset = bottomY - bottomSectionTop;
-      const topColorStyle =
-        topSectionColor === defaultColor ? "contrast" : "default";
-      const bottomColorStyle =
-        bottomSectionColor === defaultColor ? "contrast" : "default";
-
-      fillButton({
-        btn,
-        top: topColorStyle,
-        bottom: bottomColorStyle,
-        bottomOffset: offset,
-      });
-    }
-  }
-};
-
-const initBtn = (btn) => {
+const btnScrollFill = (btn) => {
   // Определяем координаты и ширину кнопки
   const btnHeight = btn.offsetHeight;
   const topY = btn.getBoundingClientRect().top;
   const bottomY = topY + btnHeight;
 
-  // Опции, передаваемые в checkCoords
-  const options = {
-    btn,
-    topY,
-    bottomY,
-  };
+  // 1. Определение фоновых секций снизу и сверху
+  const topSection = getElemFromPoint(10, topY, "section, footer");
+  const bottomSection = getElemFromPoint(10, bottomY, "section, footer");
 
-  // checkCoords(options); <--- не уверен, что это нужно
-  // !-- возможно потребуется debounce
+  if (!topSection && !bottomSection) {
+    return;
+  }
+
+  // 2. Определяем контрастность цветов секции
+  const topSectionType = getSectionType(topSection);
+  const bottomSectionType = getSectionType(bottomSection);
+  let offset = 0;
+
+  // Если цвета не равны, вычисляем порох заступа нижней секции
+  if (topSectionType !== bottomSectionType) {
+    const bottomSectionTop = bottomSection.getBoundingClientRect().top;
+    offset = bottomY - bottomSectionTop;
+  }
+
+  // Закрашиваем кнопку
+  fillButton({
+    btn,
+    offset,
+    top: topSectionType,
+    bottom: bottomSectionType,
+  });
+};
+
+const initBtn = (btn) => {
+  btnScrollFill(btn);
+
   document.addEventListener("scroll", () => {
     requestAnimationFrame(() => {
-      checkCoords(options);
+      btnScrollFill(btn);
     });
   });
 };
--- src/js/modules/helpers.js
+++ src/js/modules/helpers.js
@@ -1,7 +1,42 @@
+// Remove class or class attrubute
 export function removeClass(elem, className) {
   if (elem.classList.length === 1) {
     elem.removeAttribute("class");
   } else {
     elem.classList.remove(className);
   }
+}
+
+// Search by selector from point
+export function getElemFromPoint(x, y, selector) {
+  const elem = document.elementFromPoint(x, y);
+
+  if (elem) {
+    return elem.closest(selector);
+  }
+
+  return null;
+}
+
+// Convert RGB to hex
+export function rgbToHex(rgbString) {
+  const regex = /\d{1,3},\s*\d{1,3},\s*\d{1,3}/;
+  const rgb = rgbString.match(regex);
+
+  if (!rgb) {
+    throw new Error(`String "${rgbString}" is not RGB-value.`);
+  }
+
+  const [red, green, blue] = rgb[0].replace(/\s/g, "").split(",");
+
+  return [red, green, blue]
+    .map((val) => parseInt(val).toString(16).padStart(2, 0))
+    .join("")
+    .replace(/^/, "#");
+}
+
+// Checking if an element's background matches
+export function mathElemBackground(elem, color) {
+  const backgroundColor = rgbToHex(getComputedStyle(elem).backgroundColor);
+  return backgroundColor === color;
 }
